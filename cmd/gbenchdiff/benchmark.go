c2d3d3dc7bd7a620a3c4b58ef36bb05c64c0833f
aburdulescu <andrei.burdulescu@pm.me>
2021-09-05 10:40:44 +0300

    Add ctx checking

--- cmd/gbenchdiff/benchmark.go
+++ cmd/gbenchdiff/benchmark.go
@@ -1,7 +1,61 @@
 package main
 
+import "fmt"
+
 type Result struct {
+	Context    Context     `json:"context"`
 	Benchmarks []Benchmark `json:"benchmarks"`
+}
+
+type Context struct {
+	Date              string  `json:"date"`
+	Hostname          string  `json:"host_name"`
+	Executable        string  `json:"executable"`
+	NumCPUs           int     `json:"num_cpus"`
+	MHzPerCPU         int     `json:"mhz_per_cpu"`
+	CPUScalingEnabled bool    `json:"cpu_scaling_enabled"`
+	Caches            []Cache `json:"caches"`
+}
+
+func (c Context) Equals(other Context) error {
+	if len(c.Caches) != len(other.Caches) {
+		return fmt.Errorf("different number of CPU caches: %d vs %d", len(c.Caches), len(other.Caches))
+	}
+	for i := range c.Caches {
+		if err := c.Caches[i].Equals(other.Caches[i]); err != nil {
+			return err
+		}
+	}
+	if c.NumCPUs != other.NumCPUs {
+		return fmt.Errorf("different number of CPUs: %d vs %d", c.NumCPUs, other.NumCPUs)
+	}
+	if c.MHzPerCPU != other.MHzPerCPU {
+		return fmt.Errorf("different MHz/CPU: %d vs %d", c.MHzPerCPU, other.MHzPerCPU)
+	}
+	if c.CPUScalingEnabled != other.CPUScalingEnabled {
+		return fmt.Errorf("different CPU scaling: %d vs %d", c.CPUScalingEnabled, other.CPUScalingEnabled)
+	}
+	return nil
+}
+
+type Cache struct {
+	Type       string `json:"type"`
+	Level      int    `json:"level"`
+	Size       uint64 `json:"size"`
+	NumSharing int    `json:"num_sharing"`
+}
+
+func (c Cache) Equals(other Cache) error {
+	if c.Type != other.Type {
+		return fmt.Errorf("different type of CPU cache: %d vs %d", c.Type, other.Type)
+	}
+	if c.Level != other.Level {
+		return fmt.Errorf("different CPU cache level: %d vs %d", c.Level, other.Level)
+	}
+	if c.Size != other.Size {
+		return fmt.Errorf("different CPU cache size: %d vs %d", c.Size, other.Size)
+	}
+	return nil
 }
 
 type Benchmark struct {
